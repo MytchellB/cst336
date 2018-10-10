cc00971403fe40a59e71c579034179fd84eadd80
Mytchell Beaton <mbeaton@csumb.edu>
2018-10-10 02:32:07 +0000

    start on hw3

---
+++ hw/hw3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" /> 
+        <title>Homework 2 - CST 336</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <?php
+            include 'functions/functions.php';
+        ?>
+    </head>
+    
+    <body>
+        <main>
+            <form method="GET">
+                <input type="text" name="myText"><br>
+            </form>
+        </main>
+    </body>
+
+</html>
