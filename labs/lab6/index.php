386135a1bcadd73397b9680525a58e75f26540c6
Mytchell Beaton <mbeaton@csumb.edu>
2018-10-10 22:50:09 +0000

    lab6 work and php my admin progress

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -1,18 +1,37 @@
 <?php
-    //Creating database connection
-    $host = "localhost";
-    $dbname = "ottermart";
-    $username = "root";
-    $password = "";
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    include '../../inc/dbConnection.php';
+    $dbConn = startConnection("ottermart");
     
-    $sql = "SELECT * FROM om_category";
-    $stmt = $dbConn -> prepare($sql);
-    $stmt -> execute();
-    $records = $stmt -> fetch(); // Creates array of values.
-    print_r($records);
+    function displayCategories() {
+        global $dbConn;
+        $sql = "SELECT * FROM om_category ORDER BY catName";
+        $stmt = $dbConn -> prepare($sql);
+        $stmt -> execute();
+        $records = $stmt -> fetchAll(PDO::FETCH_ASSOC); // Creates array of values.
+        // print_r($records);
+        
+        FOREACH ($records as $record) {
+            echo "<option>" . $record['catName'] . "</option><br>";
+        }
+    }
     
+    function filterProducts(){
+        $product = $_GET['productName'];
+        global $dbConn;
+        $sql = "SELECT productName FROM om_product
+        WHERE productDescription LIKE :product ORDER BY productName";
+        
+        $namedParameters = array();
+        $namedParameters[':product'] = "%$product%";
+        
+        $stmt = $dbConn -> prepare($sql);
+        $stmt -> execute($namedParameters);
+        $records = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        
+        FOREACH ($records as $record) {
+            echo $record['productName'] . "<br>";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +42,20 @@
     </head>
     <body>
         <main>
-      	
+            <h1> Ottermart </h1>
+            <h2> Product Search </h2>
+            
+            <form>
+                Product: <input type="text" name="productName" placeholder="Product Keyword" /><br>
+                Category: 
+                <select name="category">
+                    <option value="" > Select One </option>
+                    <?= displayCategories(); ?>
+                </select>
+                <input type="submit" name="submit" value="Search!" />                
+            </form>
+            <?= filterProducts(); ?>
+      	    
         </main>
          <br/><br/>
     </body>
